9e6ed2b47c757aff54ff4481e25fcd6ed7091d3b
Gibberish314 <a.moosen@gmail.com>
2016-10-14 16:37:06 -0400

    Add database json and loop in app component with css/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,20 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
+  styleUrls: ['./app.component.css'],
   template: `
-    {{title}}
+    <h1>{{title}}</h1>
+    <div class="album" *ngFor="let album of models | async">
+      <p>{{album.title}}</p>
+      <p>By {{album.artist}}</p>
+      <img class="thumb" src="{{album.image}}">
+    </div>
   `
 })
 export class AppComponent {
+  models: FirebaseListObservable<any[]>;
   constructor(angularFire: AngularFire){
-    
+    this.models = angularFire.database.list('albums');
   }
   title = "Cameron's Music!";
 }
